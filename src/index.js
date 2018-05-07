9f197242aaa35515a8a791225560a023499fa804
Chiemi Watanabe <chiemi@slis.tsukuba.ac.jp>
2018-05-07 15:24:22 +0900

    add level and split pointer

--- src/index.js
+++ src/index.js
@@ -69,7 +69,11 @@ class Stage extends React.Component {
       var level = this.state.level;
       var pointer = this.state.pointer;
       const base = this.state.initialslots*Math.pow(2,level);
-      const hashvalue = value % base;
+      const next_base = base*2;
+      var hashvalue = value % base;
+      if(hashvalue<pointer){
+        hashvalue = value % next_base;
+      }
       var cells = [];
       var overflow = false;
       slots[hashvalue].buckets.forEach((bucket)=>{
@@ -82,24 +86,26 @@ class Stage extends React.Component {
             for(var j=0;j<bucket.cells.length;j++){
               if(!bucket.cells[j]){
                 if(i>0){overflow=true;}
-                else{slots[hashvalue].buckets[i].cells[j]=value;}
+                slots[hashvalue].buckets[i].cells[j]=value;
                 break;
               }
             }
           }
         }else{
+          slots[hashvalue].buckets.push(
+            {cells: [value,null]}
+          );
           overflow=true;
         }
         if(overflow){
+          //分割ポインタのあるところを分割する
           cells=[];
           slots[pointer].buckets.forEach((bucket)=>{
               Array.prototype.push.apply(cells,bucket.cells);
             }
           );
           cells = cells.filter(v=>v);
-          cells.push(value);
           console.log("cells="+cells.join(","));
-          const hash_base = Math.pow(2,level+1)*this.state.initialslots;
           slots.push({buckets: [
                       {cells: Array(2).fill(null)}
                      ]});
@@ -108,9 +114,10 @@ class Stage extends React.Component {
                      ]};
           console.log("pointer="+pointer);
           cells.forEach((cell)=>{
-            var index = cell%hash_base;
+            var index = cell%next_base;
             var inserted = false;
             for(var i=0;i<slots[index].buckets.length;i++){
+              console.log("index="+index);
               const bucket = slots[index].buckets[i];
               for(var j=0;j<bucket.cells.length;j++){
                 if(!bucket.cells[j]){
@@ -151,6 +158,10 @@ class Stage extends React.Component {
         <form action="javascript:void(0)" onSubmit={this.handleSubmit}>
           <input type="text" name="insertvalue" onChange={this.handleChange}/>
           <button type="submit">Insert</button>
+          <ul classname="metadata">
+            <li>Level = {this.state.level} </li>
+            <li>Split pointer = {this.state.pointer} </li>
+          </ul>
           <ol className="stage">
             {listSlots}
           </ol>
